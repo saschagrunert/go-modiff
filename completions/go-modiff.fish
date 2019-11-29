d83cb5fd6b313236975a01fa262d7d84cadedf75
Sascha Grunert <sgrunert@suse.com>
2019-11-29 10:37:59 +0100

    Update to urfave/cli v2
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- completions/go-modiff.fish
+++ completions/go-modiff.fish
@@ -9,11 +9,11 @@ function __fish_go-modiff_no_subcommand --description 'Test if there has been an
     return 0
 end
 
-complete -c go-modiff -n '__fish_go-modiff_no_subcommand' -f -l repository -s r -r -d 'repository to be used, like: github.com/owner/repo'
-complete -c go-modiff -n '__fish_go-modiff_no_subcommand' -f -l from -s f -r -d 'the start of the comparison, any valid git rev'
-complete -c go-modiff -n '__fish_go-modiff_no_subcommand' -f -l to -s t -r -d 'the end of the comparison, any valid git rev'
-complete -c go-modiff -n '__fish_go-modiff_no_subcommand' -f -l link -s l -d 'add diff links to the markdown output'
-complete -c go-modiff -n '__fish_go-modiff_no_subcommand' -f -l debug -s d -d 'enable debug output'
+complete -c go-modiff -n '__fish_go-modiff_no_subcommand' -f -l repository -r -d 'repository to be used, like: github.com/owner/repo'
+complete -c go-modiff -n '__fish_go-modiff_no_subcommand' -f -l from -r -d 'the start of the comparison, any valid git rev'
+complete -c go-modiff -n '__fish_go-modiff_no_subcommand' -f -l to -r -d 'the end of the comparison, any valid git rev'
+complete -c go-modiff -n '__fish_go-modiff_no_subcommand' -f -l link -d 'add diff links to the markdown output'
+complete -c go-modiff -n '__fish_go-modiff_no_subcommand' -f -l debug -d 'enable debug output'
 complete -c go-modiff -n '__fish_go-modiff_no_subcommand' -f -l help -s h -d 'show help'
 complete -c go-modiff -n '__fish_go-modiff_no_subcommand' -f -l version -s v -d 'print the version'
 complete -c go-modiff -n '__fish_go-modiff_no_subcommand' -f -l help -s h -d 'show help'
@@ -24,5 +24,6 @@ complete -c go-modiff -n '__fish_seen_subcommand_from docs d' -f -l markdown -d 
 complete -c go-modiff -n '__fish_seen_subcommand_from docs d' -f -l man -d 'print the man version'
 complete -c go-modiff -n '__fish_seen_subcommand_from fish f' -f -l help -s h -d 'show help'
 complete -r -c go-modiff -n '__fish_go-modiff_no_subcommand' -a 'fish f' -d 'generate the fish shell completion'
+complete -c go-modiff -n '__fish_seen_subcommand_from fish f' -f -l help -s h -d 'show help'
 complete -c go-modiff -n '__fish_seen_subcommand_from help h' -f -l help -s h -d 'show help'
 complete -r -c go-modiff -n '__fish_go-modiff_no_subcommand' -a 'help h' -d 'Shows a list of commands or help for one command'

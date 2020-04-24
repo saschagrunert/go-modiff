0c88b2076e40311e4cadf5ea1b45bfd06de174a7
Sascha Grunert <sgrunert@suse.com>
2020-04-24 12:35:38 +0200

    Bump version to v1.0.1
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- cmd/go-modiff/main.go
+++ cmd/go-modiff/main.go
@@ -23,7 +23,7 @@ func main() {
 
 	app := ccli.NewApp()
 	app.Name = "go-modiff"
-	app.Version = "1.0.0"
+	app.Version = "1.0.1"
 	app.Authors = []*cli.Author{
 		{Name: "Sascha Grunert", Email: "mail@saschagrunert.de"},
 	}
